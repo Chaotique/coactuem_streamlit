e37725d9b0d2f81bb2fb7302f33c97e68aa9c4d9
Chaotique <franzi_ska_peter@yahoo.de>
2022-02-02 09:45:51 -0500

    reimplemented reload button

--- coact_app.py
+++ coact_app.py
@@ -9,21 +9,21 @@ reload_data = st.button('Reload Data')
 st.write('Click the above button to update the displayed data (up to one hour old).')
 # load sociodem basic data
 @st.cache(suppress_st_warning=True) 
-def load_answers_basic_sociodem(reload_data=True):
+def load_answers_basic_sociodem(reload_data):
     if reload_data:
         df_sociodem_basic = pd.read_csv("answers_basic_sociodem.csv", usecols = ["genere", "edat", "pp", "p_cuid", "p_prof_si"])
     return df_sociodem_basic
-df_sociodem_basic = load_answers_basic_sociodem()
+df_sociodem_basic = load_answers_basic_sociodem(reload_data)
 
 st.write(str(len(df_sociodem_basic))+" participants already answered the sociodemografic survey (not counting the 7 OS accounts).")
 # load sociodem basic data
 
 @st.cache(suppress_st_warning=True) 
-def load_area_code_sociodem(reload_data=True):
+def load_area_code_sociodem(reload_data):
     if reload_data:
         df_sociodem_area = pd.read_csv("area_code_sociodem.csv", usecols = ["area_codi"])
     return df_sociodem_area
-df_sociodem_area = load_area_code_sociodem()
+df_sociodem_area = load_area_code_sociodem(reload_data)
 
 
 for category in df_sociodem_basic:
